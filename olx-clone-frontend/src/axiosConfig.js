c66d8d26973236c8000c54beeff3e831f18c3f02
sumitvish20 <sumitvds360@gmail.com>
2024-08-06 03:15:33 +0530

    update deployed backend url

--- olx-clone-frontend/src/axiosConfig.js
+++ olx-clone-frontend/src/axiosConfig.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000/api', // Update with your backend URL
+  baseURL: 'https://olx-clone-backend1.onrender.com', // Update with your backend URL
 });
 
 axiosInstance.interceptors.request.use(
